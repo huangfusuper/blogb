0efbe82735859cf1e8184a6b8c08d6a6eb293440
huangfu <huangpukexing>
2019-05-31 13:28:18 +0800

    增减获取ip的工具类，增加页面跳转接口

--- src/main/java/com/blog/controller/LoginController.java
+++ src/main/java/com/blog/controller/LoginController.java
@@ -1,4 +1,26 @@
 package com.blog.controller;
 
+import com.blog.utils.HttpUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 登录入口
+ * @author 皇甫
+ */
+@RestController
 public class LoginController {
+    private static final Logger LOG = LoggerFactory.getLogger(LoginController.class);
+    @GetMapping("login.html")
+    public ModelAndView login(ModelAndView modelAndView, HttpServletRequest request){
+        LOG.info("------------IP【{}】，访问登录页主界面--------------", HttpUtil.getIpAddress(request));
+        modelAndView.setViewName("/login/login");
+        return modelAndView;
+    }
+
 }
---
+++ src/main/resources/static/js/login/js/login.js
@@ -0,0 +1,35 @@
+//加载弹出层组件
+layui.use('layer',function(){
+
+    var layer = layui.layer;
+
+    //登录的点击事件
+    $("#sub").on("click",function(){
+        login();
+    })
+
+    $("body").keydown(function(){
+        if(event.keyCode == "13"){
+            login();
+        }
+    })
+
+    //登录函数
+    function login(){
+        var username = $(" input[ name='username' ] ").val();
+        var password = $(" input[ name='password' ] ").val();
+        $.ajax({
+            url:"login",
+            data:{"username":username,"password":password},
+            type:"post",
+            dataType:"json",
+            success:function(data){
+                if(data.success){
+                    window.location = "index";
+                }else{
+                    layer.msg(data.msg);
+                }
+            }
+        })
+    }
+})

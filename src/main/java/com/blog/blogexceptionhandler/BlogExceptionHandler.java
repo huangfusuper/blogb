cbbd3ec73d98bb305498a9a0e77bca686ba76a25
huangfu <huangpukexing>
2019-06-04 19:46:08 +0800

    调整部分工具类

--- src/main/java/com/blog/blogexceptionhandler/BlogExceptionHandler.java
+++ src/main/java/com/blog/blogexceptionhandler/BlogExceptionHandler.java
@@ -1,17 +1,12 @@
 package com.blog.blogexceptionhandler;
 
 
-import com.alibaba.fastjson.JSON;
 import com.blog.utils.ResponseResultVoUtil;
-import com.blog.utils.ServletUtils;
 import com.blog.utils.StringUtils;
 import com.blog.vo.ResponseResultVO;
 import org.apache.shiro.authz.UnauthorizedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 /**
  * 全局异常跳转
@@ -20,15 +15,15 @@ import javax.servlet.http.HttpServletResponse;
 @RestControllerAdvice
 public class BlogExceptionHandler {
 
+    /**
+     * 权限不足 全局异常
+     * @param e
+     * @return
+     * @throws Exception
+     */
     @ExceptionHandler(value = UnauthorizedException.class)
-    public ResponseResultVO errorHandler(HttpServletRequest reqest,
-                                         HttpServletResponse response, UnauthorizedException e) throws Exception {
-
+    public ResponseResultVO errorHandler(UnauthorizedException e) throws Exception {
         e.printStackTrace();
-
-        if(ServletUtils.isAjaxRequest(reqest)){
-            ResponseResultVoUtil.failure("BLOG565621",StringUtils.getPermissionData(e.getMessage()));
-        }
-        return ResponseResultVoUtil.failure("BLOG565622",StringUtils.getPermissionData("未知错误！"+e.getMessage()));
+        return ResponseResultVoUtil.failure("BLOG565621",StringUtils.getPermissionData(e.getMessage()));
     }
 }

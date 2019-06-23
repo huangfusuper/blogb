2c622d2a986f8226618104a8a1dcf32cec4c4648
huangfu <huangfkx>
2019-06-23 20:25:05 +0800

    test

--- src/main/java/com/blog/quarzt/MyJob.java
+++ src/main/java/com/blog/quarzt/MyJob.java
@@ -1,10 +1,14 @@
 package com.blog.quarzt;
 
 import com.blog.service.RoleService;
+import com.blog.utils.RedisUtil;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.quartz.QuartzJobBean;
+
+import java.util.UUID;
 
 /**
  * @author 皇甫
@@ -12,8 +16,15 @@ import org.springframework.scheduling.quartz.QuartzJobBean;
 public class MyJob extends QuartzJobBean {
     @Autowired
     private RoleService roleService;
+    @Autowired
+    RedisTemplate redisTemplate;
+    int i = 0;
     @Override
     protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
-        roleService.printTest("我爱昝鹏鹏");
+        Integer index = Integer.parseInt((String)redisTemplate.opsForValue().get("UUID"));
+        String uuid = (String)redisTemplate.opsForValue().getAndSet("UUID", String.valueOf(index+1));
+        System.out.println(uuid);
+
+
     }
 }
